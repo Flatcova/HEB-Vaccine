2c1f1e461ebce76c66831c584138adc677e89505
Miguel A. Covarrubias Reynoso <m_covar@hotmail.com>
2021-03-08 18:35:00 -0600

    save change

--- index.js
+++ index.js
@@ -4,20 +4,19 @@ const api = axios.get(
   "https://heb-ecom-covid-vaccine.hebdigital-prd.com/vaccine_locations.json"
 );
 
-var time = .1, interval = time*60*1000;
+var time = .3, interval = time*60*1000;
 setInterval(() => {
     api.then((res) => {
           const data = res.data.locations
           const locals = data.filter(res => res.city === 'LAREDO');
 
           locals.forEach(location => {
-              if (location.name == 'Zapata Highway H-E-B') {
+              if (location.openAppointmentSlots > 0) {
                 notifier.notify({
                   title: 'HEB-Vaccine',
                   message: `Yeahh, we found ${location.openAppointmentSlots} slots available on ${location.name}!`
                 });
               }
           });
-          // console.log(locals);
       })      
 }, interval)
